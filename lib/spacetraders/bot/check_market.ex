747a5f42ecf07cb7ed88ba5b7111c91fe9373c56
BlockListed <44610569+BlockListed@users.noreply.github.com>
2024-12-27 22:49:58 +0100

    use new route planner

--- lib/spacetraders/bot/check_market.ex
+++ lib/spacetraders/bot/check_market.ex
@@ -34,6 +34,7 @@ defmodule Spacetraders.Bot.CheckMarket do
 end
 
 defmodule Spacetraders.Bot.CheckMarket.Manager do
+  alias Spacetraders.Bot.CheckMarket.RoutePlanner
   alias Spacetraders.API
   use GenServer
 
@@ -64,19 +65,17 @@ defmodule Spacetraders.Bot.CheckMarket.Manager do
         {:ok, ship_data} = API.get_ship(ship)
         ship_data["nav"]["route"]["destination"]["systemSymbol"] == system
       end)
-
-    :ok =
-      File.write!(
-        "test_data.json",
-        Jason.encode_to_iodata!(%{markets: markets, ships: avail_ships})
-      )
+      # take at max len(markets) ships
+      |> Enum.take(Enum.count(markets))
 
     state = %{state | ships: state.ships -- avail_ships}
+
+    routes = RoutePlanner.plan_random(avail_ships, markets)
 
     {:ok, _} =
       DynamicSupervisor.start_child(
         Spacetraders.Bot.CheckMarket.SearchSupervisor,
-        {Spacetraders.Bot.CheckMarket.SearchManager, ships: avail_ships, markets: markets}
+        {Spacetraders.Bot.CheckMarket.SearchManager, routes: routes}
       )
 
     {:noreply, state}
@@ -84,6 +83,8 @@ defmodule Spacetraders.Bot.CheckMarket.Manager do
 end
 
 defmodule Spacetraders.Bot.CheckMarket.SearchManager do
+  alias Spacetraders.Market
+  alias Spacetraders.Bot.CheckMarket.RoutePlanner.ShipRoute
   alias Spacetraders.Bot.CheckMarket
   alias Spacetraders.API
   require Logger
@@ -96,12 +97,11 @@ defmodule Spacetraders.Bot.CheckMarket.SearchManager do
   end
 
   def start_link(opts) do
-    ships = Keyword.fetch!(opts, :ships)
-    markets = Keyword.fetch!(opts, :markets) |> dbg
+    routes = Keyword.fetch!(opts, :routes)
 
     with {:ok, sup} <- Supervisor.start_link([], strategy: :one_for_all),
          {:ok, task_sup} <- Supervisor.start_child(sup, Task.Supervisor) do
-      Supervisor.start_child(sup, {Task, fn -> do_search(task_sup, ships, markets) end})
+      Supervisor.start_child(sup, {Task, fn -> do_search(task_sup, routes) end})
     else
       e -> e
     end
@@ -115,80 +115,35 @@ defmodule Spacetraders.Bot.CheckMarket.SearchManager do
     }
   end
 
-  @spec do_search(pid(), [String.t()], [String.t()], [Search.t()]) :: nil
-  def do_search(sup, ships, markets, searches \\ []) do
-    cond do
-      !Enum.empty?(markets) && !Enum.empty?(ships) ->
-        [ship_to_assign | ships] = ships
-
-        ship_to_assign |> dbg
-
-        {:ok, ship} = API.get_ship(ship_to_assign)
-
-        ship_location = ship["nav"]["route"]["destination"]["symbol"] |> dbg
-
-        if ship["nav"]["status"] == "DOCKED" do
-          API.orbit_ship(ship_to_assign)
-        end
-
-        closest_market =
-          markets
-          |> Enum.map(&{&1, API.distance_between(ship_location, &1)})
-          |> Enum.min_by(&elem(&1, 1))
-          |> elem(0)
-
-        markets = markets -- [closest_market]
-
-        true = API.extract_system(ship_location) == API.extract_system(closest_market)
-
-        task = Task.Supervisor.async(sup, fn -> check_market(ship, closest_market) end)
-
-        new_search = %Search{task: task, ship: ship_to_assign}
-
-        do_search(sup, ships, markets, [new_search | searches])
-
-      !Enum.empty?(searches) ->
-        completed =
-          searches
-          |> Enum.map(& &1.task)
-          |> Task.yield_many(limit: 1, timeout: 10000)
-          |> Enum.filter(&(elem(&1, 1) != nil))
-
-        case completed do
-          [{task, {:ok, market_data}}] ->
-            Logger.info("Got market data", market_data: market_data)
-
-            Spacetraders.Market.enter_market_data(market_data)
-
-            %Search{ship: ship} = Enum.find(searches, &(&1.task == task))
-            searches = Enum.filter(searches, &(&1.task != task))
-
-            do_search(sup, [ship | ships], markets, searches)
-
-          [] ->
-            do_search(sup, ships, markets, searches)
-        end
-
-      Enum.empty?(markets) && Enum.empty?(searches) ->
-        Enum.each(ships, &CheckMarket.add_check_ship/1)
-        Logger.info("Completed search")
-        nil
-    end
+  def do_search(sup, routes) do
+    Task.Supervisor.async_stream(sup, routes, &check_route/1, concurrency: 1024, ordered: false, timeout: :infinity)
+    |> Enum.each(&Spacetraders.Bot.CheckMarket.add_check_ship/1)
   end
 
-  def check_market(ship, market) do
+  def check_route(%ShipRoute{}=route) do
+    {:ok, ship} = API.get_ship(route.ship)
     Process.sleep(API.cooldown_ms(ship))
 
-    if ship["nav"]["route"]["destination"]["symbol"] != market do
-      {:ok, nav} = API.navigate_ship(ship["symbol"], market) |> dbg
+    fly_route = fn waypoints ->
+      Enum.each(waypoints, fn wp ->
+        {:ok, nav} = API.navigate_ship(route.ship, wp)
+        {:ok, market_data} = API.get_market(wp)
+        Market.enter_market_data(market_data)
 
-      Process.sleep(API.cooldown_ms(nav))
+        Process.sleep(API.cooldown_ms(nav))
+      end)
     end
 
-    {:ok, market_data} = API.get_market(market)
+    cond do
+      Enum.fetch(route.waypoints, 0) == route.origin ->
+        {:ok, market_data} = API.get_market(route.origin)
+        Market.enter_market_data(market_data)
+        [_ | waypoints] = route.waypoints
+        fly_route.(waypoints)
+      true ->
+        fly_route.(route.waypoints)
+    end
 
-    true = Map.has_key?(market_data, "tradeGoods")
-
-    market_data
+    route.ship
   end
 end
