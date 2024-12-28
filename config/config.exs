885806034d1898dfc9392c885583ddc431f290fc
BlockListed <44610569+BlockListed@users.noreply.github.com>
2024-12-29 00:02:19 +0100

    Revert "log more stuff"
    This reverts commit 9c7981666a7ee8d08c03d6c43923cfac370d0f85.

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,6 @@ import Config
 
 config :tesla, :adapter, {Tesla.Adapter.Finch, name: Spacetraders.Finch}
 
-config :logger, level: :info, handle_sasl_reports: true
+config :logger, level: :info
 
 config :logger, :default_formatter, metadata: [:file, :line, :pid]
