e8e518d3e542a2ae0a6498e6b697a443545cf15c
BlockListed <44610569+BlockListed@users.noreply.github.com>
2024-12-28 11:57:18 +0100

    rank trade routes

--- lib/spacetraders/bot/trader/planner.ex
+++ lib/spacetraders/bot/trader/planner.ex
@@ -4,6 +4,10 @@ defmodule Spacetraders.Bot.Trader.Planner do
   defmodule Market do
     @enforce_keys [:symbol, :trade]
     defstruct [:symbol, :trade]
+
+    @type t :: %Market{symbol: String.t(), trade: map()}
+
+    @type trade_route :: {t(), t()}
   end
 
   def plan(system) do
@@ -33,8 +37,44 @@ defmodule Spacetraders.Bot.Trader.Planner do
           Map.get(import_map, trade.trade["symbol"], [])
           |> Stream.map(&{trade, &1})
         end)
-      end) |> Stream.concat() |> Stream.concat() |> Enum.to_list()
+      end)
+      |> Stream.concat()
+      |> Stream.concat()
+      |> Enum.to_list()
 
-    trade_routes |> dbg
+    highest_profit = get_best_trade_route_by(trade_routes, &get_profit_per_unit/1)
+    best_margin = get_best_trade_route_by(trade_routes, &get_margin/1)
+    highest_profit_per_dist = get_best_trade_route_by(trade_routes, &(get_profit_per_unit(&1)/get_distance(&1)))
+
+    highest_profit |> dbg
+    best_margin |> dbg
+    highest_profit_per_dist |> dbg
+  end
+
+  @spec get_best_trade_route_by([Market.trade_route()], (Market.trade_route() -> number())) ::
+          Market.trade_route()
+  def get_best_trade_route_by(trade_routes, fun) do
+    Enum.max_by(trade_routes, fun)
+  end
+
+  @spec get_profit_per_unit(Market.trade_route()) :: number()
+  def get_profit_per_unit({from, to}) do
+    to.trade["sellPrice"] - from.trade["purchasePrice"]
+  end
+
+  @spec get_margin(Market.trade_route()) :: number()
+  def get_margin({from, to}) do
+    cogs = from.trade["purchasePrice"]
+    sale_price = to.trade["sellPrice"]
+
+    (sale_price - cogs) / sale_price * 100
+  end
+
+  @spec get_distance(Market.trade_route()) :: number()
+  def get_distance({from, to}) do
+    from = from.symbol
+    to = to.symbol
+
+    API.distance_between(from, to)
   end
 end
