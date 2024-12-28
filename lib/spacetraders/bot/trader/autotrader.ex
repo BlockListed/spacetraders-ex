ec7b6c1f4d22a9100a04acb7fea7e2a16d649ee8
BlockListed <44610569+BlockListed@users.noreply.github.com>
2024-12-29 00:20:42 +0100

    add autotrader

---
+++ lib/spacetraders/bot/trader/autotrader.ex
@@ -0,0 +1,40 @@
+defmodule Spacetraders.Bot.Trader.AutoTrader do
+  use GenServer  
+
+  defmodule State do
+    @enforce_keys [:ship, :system, :funds]
+    defstruct [:ship, :system, :funds, pid: nil]
+  end
+
+  def start_link(opts) do
+    ship = Keyword.fetch!(opts, :ship)
+    system = Keyword.fetch!(opts, :system)
+    funds = Keyword.get(opts, :funds, 50_000)
+
+    GenServer.start_link(__MODULE__, {ship, system, funds}, opts)
+  end
+
+  def start_mining(%State{}=state) do
+    {:ok, pid} = Spacetraders.Bot.Trader.Manager.do_trading(state.ship, state.system, state.funds)
+
+    Process.monitor(pid)
+
+    :ok = Spacetraders.Bot.CheckMarket.check_system("X1-UY62")
+
+    %{state | pid: pid}
+  end
+
+  def init({ship, system, funds}) do
+    state = %State{ship: ship, system: system, funds: funds}
+
+    state = start_mining(state)
+
+    {:ok, state}
+  end
+
+  def handle_info({:DOWN, _, :process, _, :normal}, state) do
+    state = start_mining(state) 
+
+    {:noreply, state}
+  end
+end
