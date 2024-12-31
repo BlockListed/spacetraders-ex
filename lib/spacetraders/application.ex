cf004e0a1052b0756be187eb7961f30c16996d88
BlockListed <44610569+BlockListed@users.noreply.github.com>
2024-12-31 15:32:01 +0100

    add agent symbol to application config

--- lib/spacetraders/application.ex
+++ lib/spacetraders/application.ex
@@ -1,4 +1,11 @@
 defmodule Spacetraders.Application do
+  def configure_agent() do
+    {:ok, agent} = Spacetraders.API.agent()
+    symbol = agent["symbol"]
+
+    Application.put_env(:spacetraders, :agent, symbol)
+  end
+
   def start(_, _) do
     children = [
       {Finch, name: Spacetraders.Finch},
@@ -8,6 +15,7 @@ defmodule Spacetraders.Application do
          %Spacetraders.API.Ratelimit{limit: 2, period_ms: 1100},
          %Spacetraders.API.Ratelimit{limit: 30, period_ms: 61_000}
        ]},
+      Supervisor.child_spec({Task, &configure_agent/0}, id: Spacetraders.Agent.Configure),
       Spacetraders.Market,
       Spacetraders.API.Caching.Waypoints,
       Spacetraders.Bot.MarketChecker.Manager,
